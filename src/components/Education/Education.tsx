c489f2c8f0a4764291a60b75679c8eb0186c830c
Angel <aal51282@uga.edu>
2025-05-18 19:17:17 -0400

    Added GT information.

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -1,19 +1,37 @@
-import React from 'react';
-import styles from './Education.module.css';
+import React from "react";
+import styles from "./Education.module.css";
 
 const Education: React.FC = () => {
   return (
     <section id="education" className={styles.education} data-aos="fade-up">
       <h2>Education</h2>
-      <p>University of Georgia, Franklin College of Arts & Sciences, Athens, GA</p>
-      <p>Bachelor of Science in Computer Science, May 2025</p>
-      <p>GPA: 3.65/4.00</p>
-      <p>
-        Courses: Systems Programming, Software Engineering, Web Programming, Computer Networks, Algorithms and Data Structures
-      </p>
+
+      <div className={styles.educationItem}>
+        <h3>University of Georgia, Franklin College of Arts & Sciences</h3>
+        <p className={styles.location}>Athens, GA</p>
+        <p className={styles.degree}>
+          Bachelor of Science in Computer Science, Cum Laude
+        </p>
+        <p className={styles.dateGpa}>GPA: 3.65/4.00</p>
+        <p className={styles.courses}>
+          <strong>Courses:</strong> Systems Programming, Software Engineering,
+          Web Programming, Computer Networks, Algorithms and Data Structures
+        </p>
+      </div>
+
+      <div className={styles.educationItem}>
+        <h3>
+          Georgia Institute of Technology, Daniel Guggenheim School of Aerospace
+          Engineering
+        </h3>
+        <p className={styles.location}>Atlanta, GA</p>
+        <p className={styles.degree}>
+          Attended for Major in Aerospace Engineering (transferred to UGA)
+        </p>
+        <p className={styles.dateGpa}>GPA: 3.60/4.00</p>
+      </div>
     </section>
   );
 };
 
 export default Education;
-
